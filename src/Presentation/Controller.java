c6f6d7085c4f43c2ca626844751ff3ea7da650a4
Combii <davidstovle@gmail.com>
2016-11-01 21:58:08 +0100

    First Upload

---
+++ src/Presentation/Controller.java
@@ -0,0 +1,63 @@
+package Presentation;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Controller {
+    public Button loginButton;
+    public Button cancelButton;
+    public Label messageLabel;
+    public Button noButton;
+
+    public void clickLogin(ActionEvent actionEvent) throws IOException {
+        handleButtonAction(actionEvent);
+    }
+
+    public void clickCancel(ActionEvent actionEvent) throws IOException {
+        handleButtonAction(actionEvent);
+    }
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) throws IOException {
+        Stage stage = null;
+        Parent root = null;
+        boolean check = true;
+
+        if(event.getSource()==loginButton){
+            //get reference to the button's stage
+            stage=(Stage) loginButton.getScene().getWindow();
+            //load up OTHER FXML document
+            root = FXMLLoader.load(getClass().getResource("Presentation/pageAfterLogin.fxml"));
+        }
+        else if(event.getSource()==cancelButton){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("ALERT");
+            alert.setHeaderText("Sup");
+            alert.setContentText("You cant leave!");
+            alert.showAndWait();
+            check = false;
+        }
+        else if(event.getSource()==noButton){
+            System.exit(0);
+        }
+        if(check) {
+            //create a new scene with root and set the stage
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+    }
+
+    public void noButton(ActionEvent actionEvent) throws IOException {
+        handleButtonAction(actionEvent);
+    }
+}
